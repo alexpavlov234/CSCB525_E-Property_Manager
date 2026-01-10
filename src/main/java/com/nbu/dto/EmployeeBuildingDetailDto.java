ad85958eec93bfb2431e3d21cdff2b54d61f2d4a
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:56:12 +0200

    Add validation annotations to EmployeeBuildingDetailDto fields for employee names, building address, and numeric values

--- src/main/java/com/nbu/dto/EmployeeBuildingDetailDto.java
+++ src/main/java/com/nbu/dto/EmployeeBuildingDetailDto.java
@@ -1,5 +1,8 @@
 package com.nbu.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -9,11 +12,27 @@ import lombok.*;
 @ToString
 public class EmployeeBuildingDetailDto {
     private long employeeId;
+
+    @NotBlank(message = "First name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "First name has to start with capital letter!")
     private String employeeFirstName;
+
+    @NotBlank(message = "Middle name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Middle name has to start with capital letter!")
     private String employeeMiddleName;
+
+    @NotBlank(message = "Last name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Last name has to start with capital letter!")
     private String employeeLastName;
+
     private long buildingId;
+    @NotBlank(message = "Address cannot be blank!")
     private String buildingAddress;
+
+    @Min(value = 1, message = "Number of floors must be at least 1!")
     private int numberOfFloors;
+
+    @Min(value = 1, message = "Number of apartments must be at least 1!")
     private int numberOfApartments;
+
 }
