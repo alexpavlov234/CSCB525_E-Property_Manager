6bc2fd90dde6fdcfc830b81566183b44d248ec51
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 20:03:11 +0200

    Add validation annotations to ResidentDto fields for names, UCN, and birth date

--- src/main/java/com/nbu/dto/ResidentDto.java
+++ src/main/java/com/nbu/dto/ResidentDto.java
@@ -1,5 +1,9 @@
 package com.nbu.dto;
 
+import com.nbu.validator.ValidUCN;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -11,10 +15,25 @@ import java.time.LocalDate;
 @ToString
 public class ResidentDto {
     private long id;
+
+    @NotBlank(message = "First name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "First name has to start with capital letter!")
     private String firstName;
+
+    @NotBlank(message = "Middle name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Middle name has to start with capital letter!")
+
     private String middleName;
+
+    @NotBlank(message = "Last name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Last name has to start with capital letter!")
     private String lastName;
+
+    @NotBlank(message = "UCN cannot be blank!")
+    @ValidUCN(message = "UCN is not valid!")
     private String ucn;
+
+    @PastOrPresent(message = "Birth date cannot be in the future!")
     private LocalDate birthDate;
     private boolean useElevator;
     private long apartmentId;
