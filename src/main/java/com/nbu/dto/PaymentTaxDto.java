f620d1d4addde05d6521abccc3baa10ff7af348f
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 20:02:22 +0200

    Add validation annotations to PaymentTaxDto fields for amount and payment date

--- src/main/java/com/nbu/dto/PaymentTaxDto.java
+++ src/main/java/com/nbu/dto/PaymentTaxDto.java
@@ -1,6 +1,9 @@
 package com.nbu.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.*;
 
 import java.io.Serializable;
@@ -19,9 +22,13 @@ public class PaymentTaxDto implements Serializable {
     private long taxId;
     private long taxApartmentId;
     private long taxBuildingId;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "The amount must be greater than zero!")
     private BigDecimal amount;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Payment date cannot be null.")
+    @PastOrPresent(message = "Payment date cannot be in the future.")
     private LocalDate paymentDate;
 
     private long companyId;
