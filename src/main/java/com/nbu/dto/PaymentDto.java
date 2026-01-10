f500356833fb82c4a1788038ab9cd2ed8ef72457
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 20:00:09 +0200

    Add validation annotations to PaymentDto fields for amount and payment date

--- src/main/java/com/nbu/dto/PaymentDto.java
+++ src/main/java/com/nbu/dto/PaymentDto.java
@@ -1,5 +1,8 @@
 package com.nbu.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,7 +17,14 @@ import java.time.LocalDate;
 public class PaymentDto implements Serializable {
     private  long id;
     private  long taxId;
-    private  BigDecimal amount;
-    private  LocalDate paymentDate;
+
+    @NotNull(message = "Amount cannot be null.")
+    @Positive(message = "Amount must be positive.")
+    private BigDecimal amount;
+
+    @NotNull(message = "Payment date cannot be null.")
+    @PastOrPresent(message = "Payment date cannot be in the future.")
+    private LocalDate paymentDate;
+
     private  long employeeId;
 }
