28be8468e3e1a9763306fadcf6a2dcc2c08912cf
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:39:08 +0200

    Add validation annotations to BuildingApartmentDto fields for address, apartment, area, and owner details

--- src/main/java/com/nbu/dto/BuildingApartmentDto.java
+++ src/main/java/com/nbu/dto/BuildingApartmentDto.java
@@ -1,5 +1,6 @@
 package com.nbu.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @AllArgsConstructor
@@ -9,13 +10,30 @@ import lombok.*;
 @ToString
 public class BuildingApartmentDto {
     private long buildingId;
+
+    @NotBlank(message = "Address cannot be blank!")
     private String buildingAddress;
+
     private long apartmentId;
+
+    @Min(value = 1, message = "Apartment number must be positive!")
     private int apartmentNumber;
+
+    @Min(value = 0, message = "Floor must be zero or positive!")
     private int floor;
+
+    @NotNull(message = "Area must not be null!")
+    @Positive(message = "Area must be positive!")
     private double area;
+
     private long ownerId;
+
+    @NotBlank(message = "First name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "First name has to start with capital letter!")
+
     private String ownerFirstName;
+    @NotBlank(message = "Last name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Last name has to start with capital letter!")
     private String ownerLastName;
 }
 
