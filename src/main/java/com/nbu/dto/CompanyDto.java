cc498f0bc27311b0003862736c0146ece3f7c655
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:54:11 +0200

    Add validation annotations to CompanyDto fields for name, UIC, VAT number, and addresses

--- src/main/java/com/nbu/dto/CompanyDto.java
+++ src/main/java/com/nbu/dto/CompanyDto.java
@@ -1,6 +1,9 @@
 package com.nbu.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,13 +13,21 @@ import lombok.*;
 public class CompanyDto {
     private long id;
 
+    @NotBlank(message = "Company name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Company name has to start with capital letter!")
     private String name;
 
+    @NotBlank(message = "UIC cannot be blank!")
+    @Length(min = 9, max = 9, message = "UIC must be exactly 9 characters long!")
     private String uic;
 
+    @NotBlank(message = "VAT number cannot be blank!")
+    @Pattern(regexp = "^(BG\\d{9}|\\d{10})$", message = "VAT number must be 'BG' + 9 digits or 10 digits")
     private String vatNumber;
 
+    @NotBlank(message = "Registered address cannot be blank!")
     private String registeredAddress;
 
+    @NotBlank(message = "Mailing address cannot be blank!")
     private String mailingAddress;
 }
