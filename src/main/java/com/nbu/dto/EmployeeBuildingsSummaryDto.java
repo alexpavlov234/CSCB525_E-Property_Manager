de11e804c6a2157a6b14398ffa96533e72c2bb4e
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:57:37 +0200

    Add validation annotations to EmployeeBuildingsSummaryDto fields for names, UCN, and building count

--- src/main/java/com/nbu/dto/EmployeeBuildingsSummaryDto.java
+++ src/main/java/com/nbu/dto/EmployeeBuildingsSummaryDto.java
@@ -1,5 +1,9 @@
 package com.nbu.dto;
 
+import com.nbu.validator.ValidUCN;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @AllArgsConstructor
@@ -9,9 +13,24 @@ import lombok.*;
 @ToString
 public class EmployeeBuildingsSummaryDto {
     private long employeeId;
+
+    @NotBlank(message = "First name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "First name has to start with capital letter!")
     private String firstName;
+
+    @NotBlank(message = "Middle name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Middle name has to start with capital letter!")
+
     private String middleName;
+
+    @NotBlank(message = "Last name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Last name has to start with capital letter!")
     private String lastName;
+
+    @NotBlank(message = "UCN cannot be blank!")
+    @ValidUCN(message = "UCN is not valid!")
     private String ucn;
+
+    @Positive(message = "Building count must be positive!")
     private int buildingCount;
 }
