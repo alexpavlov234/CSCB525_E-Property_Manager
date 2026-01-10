5043ecf850b38d89f6b9c19747f4c57e95f6f867
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:37:27 +0200

    Add validation annotations to ApartmentResidentDto fields for number, floor, and resident details

--- src/main/java/com/nbu/dto/ApartmentResidentDto.java
+++ src/main/java/com/nbu/dto/ApartmentResidentDto.java
@@ -1,5 +1,9 @@
 package com.nbu.dto;
 
+import com.nbu.validator.ValidUCN;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -9,13 +13,28 @@ import lombok.*;
 @ToString
 public class ApartmentResidentDto {
     private long id;
+
+    @Min(value = 1, message = "Apartment number must be positive!")
     private int number;
+    @Min(value = 0, message = "Floor must be zero or positive!")
     private int floor;
 
     private long residentId;
+    @NotBlank(message = "First name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "First name has to start with capital letter!")
     private String firstName;
+
+    @NotBlank(message = "Middle name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Middle name has to start with capital letter!")
+
     private String middleName;
+
+    @NotBlank(message = "Last name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Last name has to start with capital letter!")
     private String lastName;
+
+    @NotBlank(message = "UCN cannot be blank!")
+    @ValidUCN(message = "UCN is not valid!")
     private String ucn;
     private boolean useElevator;
 }
