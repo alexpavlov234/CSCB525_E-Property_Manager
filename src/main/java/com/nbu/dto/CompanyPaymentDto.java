726e66163c5bc82f6278fbafa73fa9e42e420e0a
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:54:21 +0200

    Add validation annotations to CompanyPaymentDto fields for company name, total amount, year, and month

--- src/main/java/com/nbu/dto/CompanyPaymentDto.java
+++ src/main/java/com/nbu/dto/CompanyPaymentDto.java
@@ -1,5 +1,8 @@
 package com.nbu.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,8 +14,16 @@ import java.math.BigDecimal;
 @ToString
 public class CompanyPaymentDto {
     private long companyId;
+
+    @NotBlank(message = "Company name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Company name has to start with capital letter!")
     private String companyName;
+
+    @Positive(message = "Total amount of payments must be a positive number!")
     private BigDecimal totalAmountOfPayments;
+
+    @Positive(message = "Payment year must be a positive number!")
     private int paymentYear;
+    @Positive(message = "Payment month must be a positive number!")
     private int paymentMonth;
 }
