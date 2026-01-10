fa4b844c72aef5e2ab9d293b5b189fe0ce8b0de2
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 19:58:43 +0200

    Add validation annotations to EmployeeCompanyDto fields for names, UCN, company name, and UIC

--- src/main/java/com/nbu/dto/EmployeeCompanyDto.java
+++ src/main/java/com/nbu/dto/EmployeeCompanyDto.java
@@ -1,6 +1,10 @@
 package com.nbu.dto;
 
+import com.nbu.validator.ValidUCN;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -9,11 +13,31 @@ import lombok.*;
 @ToString
 public class EmployeeCompanyDto {
     private long id;
+
+    @NotBlank(message = "First name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "First name has to start with capital letter!")
     private String firstName;
+
+    @NotBlank(message = "Middle name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Middle name has to start with capital letter!")
+
     private String middleName;
+
+    @NotBlank(message = "Last name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Last name has to start with capital letter!")
     private String lastName;
+
+    @NotBlank(message = "UCN cannot be blank!")
+    @ValidUCN(message = "UCN is not valid!")
     private String ucn;
+
     private long companyId;
+
+    @NotBlank(message = "Company name cannot be blank!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Company name has to start with capital letter!")
     private String companyName;
+
+    @NotBlank(message = "UIC cannot be blank!")
+    @Length(min = 9, max = 9, message = "UIC must be exactly 9 characters long!")
     private String companyUic;
 }
