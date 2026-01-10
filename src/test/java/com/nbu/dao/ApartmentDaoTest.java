47b3f0bac8c47f7ac1cb1f3f0b8026a3e733498a
Alexander Pavlov <alexpavlov234@gmail.com>
2026-01-10 22:45:21 +0200

    Add ApartmentDaoTest and hibernate test configuration; include JUnit Jupiter dependency

---
+++ src/test/java/com/nbu/dao/ApartmentDaoTest.java
@@ -0,0 +1,33 @@
+package com.nbu.dao;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ApartmentDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createApartment() {
+    }
+
+    @Test
+    void getApartment() {
+    }
+
+    @Test
+    void updateApartment() {
+    }
+
+    @Test
+    void deleteApartment() {
+    }
+}
